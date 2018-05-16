6eaf0fc9ce7db03a37d288881b26d7a510440607
Gløer Langslet <gloer@langslet.com>
2018-05-16 11:07:02 +0200

    Eksamen v16

---
+++ eksamen/v16/oppgave3/byerOgHoteller.js
@@ -0,0 +1,42 @@
+var byer = [
+    {
+        navn: "St. Petersburg"
+    },
+    {
+        navn: "Amsterdam"
+    },
+    {
+        navn: "New York",
+        hoteller : [
+            {
+                navn: "Aurora",
+                sommerpris: 590,
+                vinterpris: 690
+            },
+            {
+                navn: "Downtown",
+                sommerpris: 660,
+                vinterpris: 750
+            },
+            {
+                navn: "City Hall",
+                sommerpris: 450,
+                vinterpris: 530
+            },
+            {
+                navn: "Wilmont",
+                sommerpris: 660,
+                vinterpris: 880
+            },
+            {
+                navn: "Chelsea inn",
+                sommerpris: 450,
+                vinterpris: 560
+            }
+
+        ]
+    },
+    {
+        navn: "Roma"
+    }
+];
